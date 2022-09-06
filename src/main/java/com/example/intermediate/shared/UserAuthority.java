49b76bbf1e92b4e1a16840c48dbf4b8bfde40027
3333812 <35902293+show5748@users.noreply.github.com>
2022-09-06 13:18:20 +0900

    Update and rename Authority.java to UserAuthority.java

---
+++ src/main/java/com/example/intermediate/shared/UserAuthority.java
@@ -0,0 +1,27 @@
+package com.example.intermediate.shared;
+
+public enum UserAuthority {
+//  ROLE_MEMBER,
+//  ROLE_GUEST
+
+  USER(Authority.USER), // 사용자 권한
+  ADMIN(Authority.ADMIN); // 관리자 권한
+
+
+  private final String authority;
+
+  UserAuthority(String authority) {
+    this.authority = authority;
+  }
+
+  public String getAuthority() {
+    return this.authority;
+  }
+
+  public static class Authority {
+
+    public static final String USER = "ROLE_USER";
+    public static final String ADMIN = "ROLE_USER";
+  }
+}
+
