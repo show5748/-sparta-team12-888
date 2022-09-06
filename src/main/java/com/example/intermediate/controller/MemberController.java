675e6c267b02f9db0f4a674d7f01d986ef82b9cb
3333812 <35902293+show5748@users.noreply.github.com>
2022-09-06 13:24:39 +0900

    Update MemberController.java

--- src/main/java/com/example/intermediate/controller/MemberController.java
+++ src/main/java/com/example/intermediate/controller/MemberController.java
@@ -2,22 +2,24 @@ package com.example.intermediate.controller;
 
 import com.example.intermediate.controller.request.LoginRequestDto;
 import com.example.intermediate.controller.request.MemberRequestDto;
+import com.example.intermediate.controller.request.NameRequestDto;
 import com.example.intermediate.controller.response.ResponseDto;
+import com.example.intermediate.service.KakaoUserService;
 import com.example.intermediate.service.MemberService;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
 public class MemberController {
 
   private final MemberService memberService;
+  private final KakaoUserService kakaoUserService;
 
   @RequestMapping(value = "/api/member/signup", method = RequestMethod.POST)
   public ResponseDto<?> signup(@RequestBody @Valid MemberRequestDto requestDto) {
@@ -40,4 +42,24 @@ public class MemberController {
   public ResponseDto<?> logout(HttpServletRequest request) {
     return memberService.logout(request);
   }
+
+  @RequestMapping(value = "/api/auth/member/mypage",method = RequestMethod.GET)
+  public ResponseDto<?> myPage(HttpServletRequest request){
+    return memberService.myPage(request);
+  }
+
+  @RequestMapping(value = "/api/auth/member/name", method = RequestMethod.POST)
+  public ResponseDto<?> updateName(@RequestBody NameRequestDto requestDto, HttpServletRequest request) {
+    return memberService.updateName(requestDto, request);
+  }
+
+
+  @GetMapping("/user/kakao/callback")
+  public ResponseDto<?> kakaoLogin(@RequestParam String code, HttpServletResponse response) throws JsonProcessingException {
+//    try { // 회원가입 진행 성공시
+    return kakaoUserService.kakaoLogin(code, response);
+//    } catch (Exception e) { // 에러나면 false
+//      throw new CustomException(ErrorCode.INVALID_KAKAO_LOGIN_ATTEMPT);
+  }
+
 }
